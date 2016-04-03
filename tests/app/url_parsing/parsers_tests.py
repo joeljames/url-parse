e7825249bcb5ffa1fcc6bff4208892e6eefb847c
Joel James <joel.james@localmed.com>
2016-04-02 21:10:12 -0500

    Add test parser

---
+++ tests/app/url_parsing/parsers_tests.py
@@ -0,0 +1,48 @@
+from nose.tools import assert_equal, assert_is_none
+from mock import patch, DEFAULT
+
+from collections import defaultdict
+from app.url_parsing import parsers
+
+
+class TestHtmlParser:
+
+    def setUp(self):
+        self.url = 'http://foo.com'
+
+    @patch.multiple('app.url_parsing.parsers',
+                    urlopen=DEFAULT,
+                    force_str=DEFAULT)
+    def test_feed(self, urlopen, force_str):
+        parsers.HtmlParser().feed(self.url)
+        urlopen.assert_called_with(self.url)
+        force_str.assert_called_with(urlopen().read())
+
+    @patch.multiple('app.url_parsing.parsers',
+                    urlopen=DEFAULT,
+                    force_str=DEFAULT)
+    def test_summary(self, urlopen, force_str):
+        force_str.return_value = '<html></html>'
+        p = parsers.HtmlParser()
+        p.feed(self.url)
+        assert_equal(p.summary['html'], 1)
+
+    @patch.multiple('app.url_parsing.parsers',
+                    urlopen=DEFAULT,
+                    force_str=DEFAULT)
+    def test_data(self, urlopen, force_str):
+        force_str.return_value = '<html></html>'
+        p = parsers.HtmlParser()
+        p.feed(self.url)
+        assert_equal(p.data, '<html></html>')
+
+    @patch.multiple('app.url_parsing.parsers',
+                    urlopen=DEFAULT,
+                    force_str=DEFAULT)
+    def test_reset(self, urlopen, force_str):
+        force_str.return_value = '<html></html>'
+        p = parsers.HtmlParser()
+        p.feed(self.url)
+        p.reset()
+        assert_is_none(p.data)
+        assert_equal(p.summary, defaultdict(int))
