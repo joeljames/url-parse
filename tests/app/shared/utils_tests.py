3abaa1ccda386d36da39001a6ce1e9fd1ac20b58
Joel James <joel.james@localmed.com>
2016-04-03 20:02:49 -0500

    Add missing tests

--- tests/app/shared/utils_tests.py
+++ tests/app/shared/utils_tests.py
@@ -1,4 +1,5 @@
 from nose.tools import assert_equal
+from mock import patch, DEFAULT
 
 from app.shared import utils
 
@@ -12,3 +13,9 @@ class TestForceString:
     def test_force_string_with_bytes(self):
         result = utils.force_str(b'abc')
         assert_equal(result, 'abc')
+
+    @patch.multiple('app.shared.utils',
+                    config=DEFAULT)
+    def test_get_config(self, config):
+        config.id = '123'
+        assert_equal(utils.get_config('id'), '123')
