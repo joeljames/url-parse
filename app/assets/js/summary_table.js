c2d20a9ea0697d63173d7a1fe7392932af7e7397
Joel James <joel.james@localmed.com>
2016-04-05 20:36:06 -0500

    Add comments to js

--- app/assets/js/summary_table.js
+++ app/assets/js/summary_table.js
@@ -1,27 +1,34 @@
 function getRandHex() {
-    return (Math.floor(Math.random() * 56) + 200).toString(16);
+  // Returns a random hex code
+  // Add a value 200 to make the hex code of light color
+  return (Math.floor(Math.random() * 56) + 200).toString(16);
 }
 
 function getRandomColor() {
-    var color = "#" + getRandHex() + getRandHex() + getRandHex();
-    if($.inArray(color, usedColor) == -1) {
-      usedColor.push(color);
-      return color
-    } else {
-      getRandomColor();
-    }
+  // Return a color every time this method is called
+  // Keeps a track of previously returned color so that
+  // next time this method is called a new unique color is returned.
+  var color = "#" + getRandHex() + getRandHex() + getRandHex();
+  if($.inArray(color, usedColor) == -1) {
+    usedColor.push(color);
+    return color
+  } else {
+    getRandomColor();
+  }
 }
 
 function RegisterTableClickEvent() {
-  // Global used color list
+  // usedColor arry keeps a track of previously used color.
   usedColor = new Array();
-  $("#summaryTable").on("click", "td", function() {
-    var value = $.trim($(this).text());
-    var sourceDataElement = document.getElementById("sourceData");
-    var sourceDataContent = document.getElementById("sourceData").innerHTML;
-    var color = getRandomColor();
+    $("#summaryTable").on("click", "td", function() {
+      var value = $.trim($(this).text());
+      var sourceDataElement = document.getElementById("sourceData");
+      var sourceDataContent = document.getElementById("sourceData").innerHTML;
+      var color = getRandomColor();
+      // Regex to capture a tag
+      var tagRegex = RegExp("&lt;\/?(" + value + ")", "g");
     sourceDataElement.innerHTML =  sourceDataContent.replace(
-      RegExp("&lt;\/?(" + value + ")", "g"),
+      tagRegex,
       '&lt;<strong><span style="background-color:' + color + '">$1</span></strong>'
     );
   });
--- app/assets/js/summary_table_bundle.js
+++ app/assets/js/summary_table_bundle.js
@@ -1,28 +1,35 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 function getRandHex() {
-    return (Math.floor(Math.random() * 56) + 200).toString(16);
+  // Returns a random hex code
+  // Add a value 200 to make the hex code of light color
+  return (Math.floor(Math.random() * 56) + 200).toString(16);
 }
 
 function getRandomColor() {
-    var color = "#" + getRandHex() + getRandHex() + getRandHex();
-    if($.inArray(color, usedColor) == -1) {
-      usedColor.push(color);
-      return color
-    } else {
-      getRandomColor();
-    }
+  // Return a color every time this method is called
+  // Keeps a track of previously returned color so that
+  // next time this method is called a new unique color is returned.
+  var color = "#" + getRandHex() + getRandHex() + getRandHex();
+  if($.inArray(color, usedColor) == -1) {
+    usedColor.push(color);
+    return color
+  } else {
+    getRandomColor();
+  }
 }
 
 function RegisterTableClickEvent() {
-  // Global used color list
+  // usedColor arry keeps a track of previously used color.
   usedColor = new Array();
-  $("#summaryTable").on("click", "td", function() {
-    var value = $.trim($(this).text());
-    var sourceDataElement = document.getElementById("sourceData");
-    var sourceDataContent = document.getElementById("sourceData").innerHTML;
-    var color = getRandomColor();
+    $("#summaryTable").on("click", "td", function() {
+      var value = $.trim($(this).text());
+      var sourceDataElement = document.getElementById("sourceData");
+      var sourceDataContent = document.getElementById("sourceData").innerHTML;
+      var color = getRandomColor();
+      // Regex to capture a tag
+      var tagRegex = RegExp("&lt;\/?(" + value + ")", "g");
     sourceDataElement.innerHTML =  sourceDataContent.replace(
-      RegExp("&lt;\/?(" + value + ")", "g"),
+      tagRegex,
       '&lt;<strong><span style="background-color:' + color + '">$1</span></strong>'
     );
   });
